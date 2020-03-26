0faf8f1dbf4166c838418a63d5f805475ed5285a
Mark King <markpkng@gmail.com>
2020-03-25 22:07:08 -0400

    4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,14 +3,14 @@
 import sys
 
 
-ADD = 0b10100000
+ADD = 0b10100000  # Add registry addresses together
 # AND = 0b10101000
-# CALL = 0b1010000
+CALL = 0b1010000  # Jump to subroutine's address
 # CMP = 0b10100111
-DEC = 0b01100110
+DEC = 0b01100110  # Decrement register
 # DIV = 0b10100011
-HLT = 0b00000001
-INC = 0b01100101
+HLT = 0b00000001  # Halt the program
+INC = 0b01100101  # Increment register
 # INT = 0b01010010
 # IRET = 0b00010011
 # JEQ = 0b01010101
@@ -21,22 +21,24 @@ INC = 0b01100101
 # JMP = 0b01010100
 # JNE = 0b01010110
 # LD = 0b10000011
-LDI = 0b10000010
+LDI = 0b10000010  # Set specific register to specific value
 # MOD = 0b10100100
-MUL = 0b10100010
+MUL = 0b10100010  # Multiply two registers together and store result in register A
 # NOP = 0b00000000
 # NOT = 0b01101001
 # OR = 0b10101010
-POP = 0b01000110
+POP = 0b01000110  # Pop insturctions off of the stack
 # PRA = 0b01001000
-PRN = 0b01000111
-PUSH = 0b01000101
-# RET = 0b00010001
+PRN = 0b01000111  # Print number
+PUSH = 0b01000101  # Push instructions onto the stack
+RET = 0b00010001  # Go to return address after subroutine
 # SHL = 0b10101100
 # SHR = 0b10101101
 # ST = 0b10000100
-SUB = 0b10100001
+SUB = 0b10100001  # Substract register A from register B and store result in register A
 # XOR = 0b10101011
+
+SP = 7
 
 
 class CPU:
@@ -47,48 +49,20 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        self.sp = 7
-        self.reg[self.sp] = 0xF4
-        # self.fl = 0
-        # self.ie = 0
-
+        self.reg[SP] = 0xF4
         self.branchtable = {}
-        # self.branchtable[CALL] = self.handle_call
+        self.branchtable[CALL] = self.handle_call
         self.branchtable[HLT] = self.handle_hlt
-        # self.branchtable[INT] = self.handle_int
-        # self.branchtable[IRET] = self.handle_iret
-        # self.branchtable[JEQ] = self.handle_jeq
-        # self.branchtable[JGE] = self.handle_jge
-        # self.branchtable[JGT] = self.handle_jgt
-        # self.branchtable[JLE] = self.handle_jle
-        # self.branchtable[JLT] = self.handle_jlt
-        # self.branchtable[JMP] = self.handle_jmp
-        # self.branchtable[JNE] = self.handle_jne
-        # self.branchtable[LD] = self.handle_ld
         self.branchtable[LDI] = self.handle_ldi
-        # self.branchtable[MUL] = self.handle_mul
-        # self.branchtable[NOP] = self.handle_nop
         self.branchtable[POP] = self.handle_pop
-        # self.branchtable[PRA] = self.handle_pra
         self.branchtable[PRN] = self.handle_prn
         self.branchtable[PUSH] = self.handle_push
-        # self.branchtable[RET] = self.handle_ret
-        # self.branchtable[ST] = self.handle_st
-
+        self.branchtable[RET] = self.handle_ret
         self.branchtable[ADD] = self.alu_handle_add
-        # self.branchtable[AND] = self.alu_handle_and
-        # self.branchtable[CMP] = self.alu_handle_cmp
         self.branchtable[DEC] = self.alu_handle_dec
-        # self.branchtable[DIV] = self.alu_handle_div
         self.branchtable[INC] = self.alu_handle_inc
-        # self.branchtable[MOD] = self.alu_handle_mod
         self.branchtable[MUL] = self.alu_handle_mul
-        # self.branchtable[NOT] = self.alu_handle_not
-        # self.branchtable[OR] = self.alu_handle_or
-        # self.branchtable[SHL] = self.alu_handle_shl
-        # self.branchtable[SHR] = self.alu_handle_shr
         self.branchtable[SUB] = self.alu_handle_sub
-        # self.branchtable[XOR] = self.alu_handle_xor
 
     def ram_read(self, mar):
         return self.ram[mar]
@@ -96,12 +70,12 @@ class CPU:
     def ram_write(self, mdr, mar):
         self.ram[mar] = mdr
 
-    def load(self, program):
+    def load(self, path):
         """Load a program into memory."""
         try:
             address = 0
-            with open(program) as f:
-                for line in f:
+            with open(path) as file:
+                for line in file:
                     line = line.split('#')[0]
                     line = line.strip()
                     if line == '':
@@ -110,16 +84,8 @@ class CPU:
                     self.ram[address] = instruction
                     address += 1
         except FileNotFoundError:
-            print('ERROR: Must have valid file name')
+            print('File not found.')
             sys.exit(2)
-
-    # def alu(self, op, reg_a, reg_b):
-    #     """ALU operations."""
-
-    #     if op == ADD:
-    #         self.reg[reg_a] += self.reg[reg_b]
-    #     else:
-    #         raise Exception("Unsupported ALU operation")
 
     def trace(self):
         """
@@ -129,7 +95,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -143,46 +108,75 @@ class CPU:
     def run(self):
         """Run the CPU."""
         while True:
-            ir = self.ram[self.pc]
+            ir = self.ram[self.pc]  # instruction register
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
-            operand_count = ir >> 6
-            instruction_length = operand_count + 1
 
             self.branchtable[ir](operand_a, operand_b)
+            if not ir >> 4 & 0b0001:
+                self.pc += (ir >> 6) + 1
 
-            self.pc += instruction_length
+    # General Instructions
 
-    def handle_hlt(self, a, b):
+    def handle_call(self, a, b):
+        # Decrement SP
+        self.reg[SP] -= 1
+
+        # Push return address on stack
+        return_address = self.pc + 2
+        self.ram[self.reg[SP]] = return_address
+
+        # Set PC to value in register
+        reg_num = self.ram[self.pc + 1]
+        self.pc = self.reg[reg_num]
+
+    def handle_hlt(self, *_):
         sys.exit(0)
 
-    def handle_ldi(self, a, b):
-        self.reg[a] = b
+    def handle_ldi(self, reg_a, reg_b):
+        self.reg[reg_a] = reg_b
 
-    def handle_pop(self, reg_num, b):
-        val = self.ram[self.reg[self.sp]]
+    def handle_pop(self, reg_num, _):
+        # Get value from address pointed to by SP
+        val = self.ram[self.reg[SP]]
+
+        # Copy to given register
         self.reg[reg_num] = val
-        self.reg[self.sp] += 1
 
-    def handle_prn(self, a, b):
-        print(self.reg[a])
+        # Increment SP
+        self.reg[SP] += 1
 
-    def handle_push(self, reg_num, b):
-        self.reg[self.sp] -= 1
+    def handle_prn(self, reg_num, _):
+        print(self.reg[reg_num])
+
+    def handle_push(self, reg_num, _):
+        # Decrement SP
+        self.reg[SP] -= 1
+
+        # Get value in given register
         reg_val = self.reg[reg_num]
-        self.ram[self.reg[self.sp]] = reg_val
 
-    def alu_handle_add(self, a, b):
-        self.reg[a] += self.reg[b]
+        # Copy value to address pointed to by SP
+        self.ram[self.reg[SP]] = reg_val
 
-    def alu_handle_dec(self, a, b):
-        self.reg[a] -= 1
+    def handle_ret(self, *_):
+        # Pop return address off stack and store it on PC
+        self.pc = self.ram[self.reg[SP]]
+        self.reg[SP] += 1
 
-    def alu_handle_inc(self, a, b):
-        self.reg[a] += 1
+    def alu_handle_add(self, reg_a, reg_b):
+        self.reg[reg_a] += self.reg[reg_b]
 
-    def alu_handle_mul(self, a, b):
-        self.reg[a] *= self.reg[b]
+    # ALU Instructions
 
-    def alu_handle_sub(self, a, b):
-        self.reg[a] -= self.reg[b]
+    def alu_handle_dec(self, reg_num, _):
+        self.reg[reg_num] -= 1
+
+    def alu_handle_inc(self, reg_num, _):
+        self.reg[reg_num] += 1
+
+    def alu_handle_mul(self, reg_a, reg_b):
+        self.reg[reg_a] *= self.reg[reg_b]
+
+    def alu_handle_sub(self, reg_a, reg_b):
+        self.reg[reg_a] -= self.reg[reg_b]
